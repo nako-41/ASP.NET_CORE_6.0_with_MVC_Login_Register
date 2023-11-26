4e4a5c49e7b70673207361704323c88f786adf4c
okan kaipoglu <okan_kaipoglu@outlook.com>
2023-11-27 01:42:14 +0300

    toastr.js kutuphanesi kullanılıp parola veya isim degisikligi icin kullanıcı bilgilendirme daha guzel hale getirildi

--- ASP.NET CORE_6.0_with_MVC_Login_Register/Controllers/AccountController.cs
+++ ASP.NET CORE_6.0_with_MVC_Login_Register/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using NETCore.Encrypt.Extensions;
+using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 
 namespace ASP.NET_CORE_6._0_with_MVC_Login_Register.Controllers
@@ -33,12 +34,10 @@ namespace ASP.NET_CORE_6._0_with_MVC_Login_Register.Controllers
         {
             if (ModelState.IsValid)
             {
-                string md5salt = _configuration.GetValue<string>("AppSettings:MD5Salt");
-                string saltedpassword = model.Password + md5salt;
-                string hashedpass = saltedpassword.MD5();
+                string hashedpass = DoMD5HashedString(model.Password);
 
                 User user = _databaseContext.Users.SingleOrDefault(x => x.Username.ToLower() == model.Username.ToLower() && x.Password == hashedpass);
-                
+
                 if (user != null)
                 {
                     if (user.Locked)
@@ -47,35 +46,42 @@ namespace ASP.NET_CORE_6._0_with_MVC_Login_Register.Controllers
                         return View(model);
                     }
                 }
+
                 else
                 {
-                    ModelState.AddModelError("","Username or password is incorrect.");
+                    ModelState.AddModelError("", "Username or password is incorrect.");
                 }
 
                 List<Claim> claims = new List<Claim>();
                 claims.Add(new Claim(ClaimTypes.NameIdentifier, user.ID.ToString()));
                 claims.Add(new Claim(ClaimTypes.Name, user.FullName ?? string.Empty));
                 claims.Add(new Claim(ClaimTypes.Role, user.Role));
-				claims.Add(new Claim("Username", user.Username));
+                claims.Add(new Claim("Username", user.Username));
 
 
-
-                ClaimsIdentity identity=new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);   
+                ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
                 ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
 
                 HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
 
 
-                return RedirectToAction("Index","Home");
-
+                return RedirectToAction("Index", "Home");
 
             }
 
             return View(model);
         }
 
-		[AllowAnonymous]
+        private string DoMD5HashedString(string s)
+        {
+            string md5salt = _configuration.GetValue<string>("AppSettings:MD5Salt");
+            string salted =s+ md5salt;
+            string hashed = salted.MD5();
+            return hashed;
+        }
+
+        [AllowAnonymous]
 		public IActionResult Register()
         {
             return View();
@@ -93,9 +99,11 @@ namespace ASP.NET_CORE_6._0_with_MVC_Login_Register.Controllers
                 }
 
 
-                string md5salt = _configuration.GetValue<string>("AppSettings:MD5Salt");
-                string saltedpassword = model.Password + md5salt;
-                string hashedpass = saltedpassword.MD5();
+                //string md5salt = _configuration.GetValue<string>("AppSettings:MD5Salt");
+                //string saltedpassword = model.Password + md5salt;
+                //string hashedpass = saltedpassword.MD5();
+
+                string hashedpass = DoMD5HashedString(model.Password);
 
                 User user = new()
                 {
@@ -111,8 +119,59 @@ namespace ASP.NET_CORE_6._0_with_MVC_Login_Register.Controllers
 
         public IActionResult Profile()
         {
+            ProfileInfoLoader();
+
             return View();
         }
+
+        private void ProfileInfoLoader()
+        {
+            Guid userid = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            User user = _databaseContext.Users.FirstOrDefault(x => x.ID == userid);
+
+            ViewData["fullname"] = user.FullName;
+        }
+
+        [HttpPost]
+        public IActionResult ProfileChangeFullName([Required][StringLength(50)] string? fullname)
+        {
+            if (ModelState.IsValid)
+            {
+              Guid userid = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                User user = _databaseContext.Users.FirstOrDefault(x => x.ID == userid);
+
+                user.FullName = fullname;
+                _databaseContext.SaveChanges();
+
+                return RedirectToAction(nameof(Profile));
+            }
+
+
+            return View("Profile");
+        }
+
+        [HttpPost]
+        public IActionResult ProfileChangePassword([Required][MinLength(6)][MaxLength(16)] string? password)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid userid = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                User user = _databaseContext.Users.FirstOrDefault(x => x.ID == userid);
+
+                string hashedpassword = DoMD5HashedString(password);
+
+                user.Password = hashedpassword;
+                _databaseContext.SaveChanges();
+
+                ViewData["result"] = "PasswordChanged";
+
+                //return RedirectToAction(nameof(Profile));
+            }
+
+            ProfileInfoLoader();
+            return View("Profile");
+        }
+
         public IActionResult Logout()
         {
             HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
--- ASP.NET CORE_6.0_with_MVC_Login_Register/wwwroot/js/site.js
+++ ASP.NET CORE_6.0_with_MVC_Login_Register/wwwroot/js/site.js
@@ -1,4 +1,16 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": true,
+    "showDuration": "400",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
