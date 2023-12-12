9d49bb92516d56d32a767dd3a4fb88e2b98dbaa0
okan kaipoglu <okan_kaipoglu@outlook.com>
2023-12-12 16:49:53 +0300

    ajax'in Unobtrusive aracını kullanıp verileri sayfa yuklendiginde cekme islemi yapildi

---
+++ ASP.NET CORE_6.0_with_MVC_Login_Register/Controllers/MemberController.cs
@@ -0,0 +1,34 @@
+﻿using ASP.NET_CORE_6._0_with_MVC_Login_Register.Context;
+using ASP.NET_CORE_6._0_with_MVC_Login_Register.Entities;
+using ASP.NET_CORE_6._0_with_MVC_Login_Register.Models.ViewModels;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_CORE_6._0_with_MVC_Login_Register.Controllers
+{
+    public class MemberController : Controller
+    {
+        private readonly DatabaseContext _databaseContext;
+        private readonly IMapper _mapper;
+
+        public MemberController(DatabaseContext databaseContext, IMapper mapper)
+        {
+            _databaseContext = databaseContext;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MemberListPartial() 
+        {
+            List<User> users = _databaseContext.Users.ToList();
+
+            List<UserModel> model = users.Select(x => _mapper.Map<UserModel>(x)).ToList();
+
+            return PartialView("_MemberListPartial", model);
+
+        }
+    }
+}
